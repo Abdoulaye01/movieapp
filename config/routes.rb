bfe8a354c28fe486078f5b0b30e3be73d1583aac
unknown <abdoulayekaloga059@gmail.com>
2018-02-12 15:06:26 +0000

    added loginlogout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,23 @@
  Rails.application.routes.draw do
+  get 'pages/index'
+
+  get 'pages/about'
+
+  #get 'sessions/new'
+
+  #get 'sessions/create'
+
+  #get 'sessions/destroy'
+controller :sessions do
+get 'login' => :new 
+post 'login' => :create
+get 'logout' => :destroy
+delete 'logout' => :destroy
+
+end
+  resources :users
   resources :genres
   resources :movies
-  root 'movies#index'
+  root 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
